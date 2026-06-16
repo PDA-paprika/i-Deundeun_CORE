0494dbc2045462ff4b426b35f729b1cd96138ac1
shinyj0 <yejins0605@gmail.com>
2026-06-16 21:01:24 +0900

    feat: 자녀 repository 구현

---
+++ src/main/java/com/iduenduen/coreservice/domain/children/repository/ChildrenRepository.java
@@ -0,0 +1,8 @@
+package com.iduenduen.coreservice.domain.children.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.iduenduen.coreservice.domain.children.entity.Children;
+
+public interface ChildrenRepository extends JpaRepository<Children, Long> {
+}
