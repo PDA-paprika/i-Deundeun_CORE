43356831868c43bab23492b488a745eef197e062
dk7648 <dk7648@korea.ac.kr>
2026-06-24 12:54:43 +0900

    feat: setter 필드 추가

--- src/main/java/com/iduenduen/coreservice/domain/parent/dto/ClusterTrainResponse.java
+++ src/main/java/com/iduenduen/coreservice/domain/parent/dto/ClusterTrainResponse.java
@@ -3,8 +3,10 @@ package com.iduenduen.coreservice.domain.parent.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class ClusterTrainResponse {
 
