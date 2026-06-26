9a027a75600a64053c7acf18aa4d4d1deca47b9d
sonnny0625 <andyson0205@gmail.com>
2026-06-26 17:05:46 +0900

    fix- sql문 수정 external_etf_id -> etf_id

--- src/main/resources/sql/account_etf_histories.sql
+++ src/main/resources/sql/account_etf_histories.sql
@@ -4,7 +4,7 @@ CREATE TABLE `account_etf_histories`
     `id`              BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `account_id`      BIGINT       NOT NULL,
     `event_type`      VARCHAR(20)  NOT NULL COMMENT 'BUY | SELL | GIFT_ETF_OUT | GIFT_ETF_IN',
-    `external_etf_id` VARCHAR(100) NOT NULL COMMENT 'market schema etfs.id',
+    `etf_id`          BIGINT       NOT NULL,
     `qty_delta`       INT          NOT NULL COMMENT 'ETF 수량 변화 (+매수·입고 / -매도·출고)',
     `price`           BIGINT       NOT NULL COMMENT 'ETF 체결 또는 양도 단가',
     `reference_id`    VARCHAR(100) NULL     COMMENT 'gift_transfers.id or external execution id',
