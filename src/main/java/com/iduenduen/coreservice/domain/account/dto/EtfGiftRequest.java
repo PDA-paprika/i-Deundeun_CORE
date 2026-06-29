74e834302c050128ef40d1661faa227da3bf54c3
dk7648 <dk7648@korea.ac.kr>
2026-06-29 19:06:27 +0900

    feat: 증여 관련 dto 정의

---
+++ src/main/java/com/iduenduen/coreservice/domain/account/dto/EtfGiftRequest.java
@@ -0,0 +1,25 @@
+package com.iduenduen.coreservice.domain.account.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+
+public record EtfGiftRequest(
+
+    @NotNull
+    @JsonProperty("child_id")
+    Long childId,
+
+    @NotNull
+    @JsonProperty("etf_id")
+    Long etfId,
+
+    @JsonProperty("etf_name")
+    String etfName,
+
+    @Positive
+    int qty,
+
+    String memo
+) {}
